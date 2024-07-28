4200b9fbbc317b2ed98456089e1b8104344cdfec
William With <william@wizdo.se>
2024-07-28 13:00:53 +0200

    init

---
+++ src/SocketContext.js
@@ -0,0 +1,24 @@
+// src/SocketContext.js
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import io from 'socket.io-client';
+
+const SocketContext = createContext();
+
+export const useSocket = () => useContext(SocketContext);
+
+export const SocketProvider = ({ children }) => {
+  const [socket, setSocket] = useState(null);
+  const port = 5050;
+
+  useEffect(() => {
+    const newSocket = io('http://localhost:' + port); // Adjust the backend URL as needed
+    setSocket(newSocket);
+    return () => newSocket.close();
+  }, []);
+
+  return (
+    <SocketContext.Provider value={socket}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
